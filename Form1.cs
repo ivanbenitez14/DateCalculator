3b8d1ac3f4878e9759c8cdde5371f4b5aec03051
ivanbenitez14 <60349854+ivanbenitez14@users.noreply.github.com>
2020-01-29 22:49:55 -0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DiferenciaDeFecha
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CalcularDiferencia();
+        }
+
+        void CalcularDiferencia()
+        {
+            DateTime Fecha1 = dateTimePicker1.Value.Date;
+            DateTime Fecha2 = dateTimePicker2.Value.Date;
+
+            TimeSpan tSpan = Fecha1 - Fecha2;
+
+            int Dias = tSpan.Days;
+
+            textBox1.Text = Dias.ToString();
+
+        }
+
+    }
+}
